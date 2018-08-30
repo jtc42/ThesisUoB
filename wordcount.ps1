6be6330bac67150429581033cff19670ee39c49a
jtc42 <jtc9242@gmail.com>
2018-08-30 16:45:06 +0100

    Script to obtain plot of historical wordcount

---
+++ wordcount.ps1
@@ -0,0 +1,35 @@
+$hashes = git rev-list --all
+$outfile = "./wordcount.txt"
+
+Clear-Content $outfile 
+
+ForEach ($hash in $hashes)
+{
+    echo "Operating on hash: " $hash
+    echo "Checking out..."
+    git checkout $hash
+
+    echo "Word-counting..."
+    <# Get full wordcount.pl output #> 
+    $countfull = texcount main.tex -inc -total
+
+    <# Pull out the second line (index 1) containing word count #> 
+    $countline = $countfull | Select -Index 1
+
+    <# Pull out the number, stripping text #> 
+    $n = $countline.Substring($countline.IndexOf(':')+2) -replace '(?:\r|\n)',''
+    echo $n
+
+    <# Get datetime of commit #>
+    $dateline = git log -2 --pretty=tformat:%aD | Select -Index 0
+    echo "DATELINE: " $dateline
+    $datetime = $dateline -replace '(?:\r|\n)',''
+    echo $datetime
+
+    $outline = $datetime + "; " + $n
+
+    echo $outline | Out-File -Append $outfile -Encoding ASCII
+}
+
+<# Revert back to latest #> 
+git checkout master
---
+++ wordplot.py
@@ -0,0 +1,36 @@
+import numpy as np
+import csv
+import codecs
+from datetime import datetime
+
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+
+filename = "./wordcount.txt"
+
+dates = []
+words = []
+
+with open(filename, 'r') as csvfile:
+    csv_reader = csv.reader(csvfile, delimiter=';')
+    for row in csv_reader:
+        print(row)
+        try:
+            words.append(int(row[1]))
+            dates.append(datetime.strptime(row[0], '%a, %d %b %Y %H:%M:%S %z'))
+        except ValueError:
+            print("Skipping broken wordcount results")
+        
+
+plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%d/%m/%Y'))
+#plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+
+plt.plot(dates, words)
+plt.gcf().autofmt_xdate()
+
+plt.ylabel("Words")
+plt.title("Thesis word count from git history")
+
+plt.show()
+
+print("END")
